f5983d85a9f9c7d35b92244d8f24e8b5470f3bf6
Carleigh Crockett <capleugh@gmail.com>
2020-01-03 20:52:17 -0700

    Update factories

--- spec/factories/book.rb
+++ spec/factories/book.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :book do
     sequence(:title) { |n| "Title #{n}" }
+    sequence(:publication_year, 1936) { |n| "#{n}" }
+    sequence(:pages, 350) { |n| "#{n}" }
+    author
   end
 end
